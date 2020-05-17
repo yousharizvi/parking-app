e42efcd6757604f131a66eb06d83852cf7d615e4
Yousha Rizvi <yousharizvi@gmail.com>
2020-05-17 18:49:22 +0500

    Show bookings with slots

--- parkings/views.py
+++ parkings/views.py
@@ -10,6 +10,7 @@ from parkings.forms import ParkingSearchForm
 from feedbacks.models import Feedback
 from common.decorators import login_required, is_superuser, is_not_superuser
 from datetime import datetime
+from bson.objectid import ObjectId
 
 
 parkings_app = Blueprint('parkings_app', __name__)
@@ -68,7 +69,22 @@ def book(parking_id):
         }
     ]
     parking = Parking.objects.get(pk=parking_id)
-    available_slots = ParkingSlot.objects.filter(parking_id=parking_id)
+    pipeline = [
+        {
+            '$match': {
+                'parking_id': ObjectId(parking_id)
+            }
+        },
+        {
+            '$lookup': {
+                'from': 'booking',
+                'localField': '_id',
+                'foreignField': 'parkingslot_id',
+                'as': 'bookings'
+            }
+        }
+    ]
+    available_slots = ParkingSlot.objects.aggregate(pipeline)
     return render_template('parking/book.html', form=form, parking_slots=available_slots, parking=parking)
 
 
@@ -90,5 +106,6 @@ def bookings():
 def cancel(booking_id):
     booking = Booking.objects.get(pk=booking_id)
     if g.user.is_superuser or g.user.pk == booking.user_id:
-        bookings = Booking.objects(pk=booking_id).update(set__is_cancelled=True)
+        bookings = Booking.objects(
+            pk=booking_id).update(set__is_cancelled=True)
     return redirect(url_for('parkings_app.bookings'))
