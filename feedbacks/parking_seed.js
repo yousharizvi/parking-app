cd020e91006a9f1f794bf3c457501427b5771bd9
Yousha Rizvi <yousharizvi@gmail.com>
2020-05-19 17:16:47 +0500

    Move parking seed script

--- feedbacks/parking_seed.js
+++ feedbacks/parking_seed.js
@@ -1,6 +1,7 @@
 for (var i = 0; i < 10; i++) {
     db.parking.insert({ display_name: 'Parking ' + (i + 1), address: 'Address for parking ' + (i + 1) })
 }
+
 db.parking.find().toArray().forEach((parking) => {
     for (var i = 0; i < 100; i++) {
         db.parkingslot.insert({
